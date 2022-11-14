1a348a5b0ddd7b86a4db7c91735677f68041f2f9
Hugocod <u.medioli@icloud.com>
2022-11-14 16:52:03 +0100

    added vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <example-component />
+        ciao
+    </div>
+</template>
+
+<script>
+import ExampleComponent from "../components/ExampleComponent.vue";
+
+export default {
+    name: "App",
+    components: { ExampleComponent },
+};
+</script>
+
+<style scoped lang="scss"></style>
