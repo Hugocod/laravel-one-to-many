f4c2d204e9251b5c2933e67a680d6648de5b59ee
Hugocod <u.medioli@icloud.com>
2022-11-17 20:36:14 +0100

    migrate post category one to many

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -100,11 +100,13 @@ class PostController extends Controller
         //
          $request->validate([
             'title' => 'required|min:5|max:255',
-            'content' => 'required'
+            'content' => 'required',
+            'category_id' => 'nullable|exists: categories,id'
          ], [
             'required' => ':attribute is mandatory',
             'min' => ':attribute should be at least :min chars',
-            'max' => ':attribute should have max length of :max chars'
+            'max' => ':attribute should have max length of :max chars',
+            'category_id.exists' => 'Category does not exists anymore'
          ]);
         $form_data = $request->all();
         if($post->title != $form_data['title']){
